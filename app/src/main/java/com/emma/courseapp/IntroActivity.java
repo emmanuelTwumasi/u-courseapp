aeafb055b8de1cc34577253fbb5d028f636ba20a
emmanuelTwumasi <protwumasi@gmail.com>
2024-06-18 05:24:15 +0000

    add many changes

--- app/src/main/java/com/emma/courseapp/IntroActivity.java
+++ app/src/main/java/com/emma/courseapp/IntroActivity.java
@@ -1,24 +1,66 @@
 package com.emma.courseapp;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AnticipateInterpolator;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.splashscreen.SplashScreen;
+
+import com.emma.courseapp.databinding.ActivityIntroBinding;
 
 public class IntroActivity extends AppCompatActivity {
+
+    ActivityIntroBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_intro);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        splashScreen.setKeepOnScreenCondition(() -> {
+            // Keep the splash screen visible until your app is ready to display content.
+            return false;
         });
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+//            getSplashScreen().setOnExitAnimationListener(splashScreenView -> {
+//                final ObjectAnimator slideUp = ObjectAnimator.ofFloat(
+//                        splashScreenView,
+//                        View.TRANSLATION_Y,
+//                        0f,
+//                        -splashScreenView.getHeight()
+//                );
+//                slideUp.setInterpolator(new AnticipateInterpolator());
+//                slideUp.setDuration(2L);
+//
+//                // Call SplashScreenView.remove at the end of your custom animation.
+//                slideUp.addListener(new AnimatorListenerAdapter() {
+//                    @Override
+//                    public void onAnimationEnd(Animator animation) {
+//                        splashScreenView.remove();
+//                    }
+//                });
+//
+//                // Run your animation.
+//                slideUp.start();
+//            });
+//        }
+
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
     }
 }
---
+++ app/src/main/java/com/emma/courseapp/adapter/CourseViewHolder.java
@@ -0,0 +1,12 @@
+package com.emma.courseapp.adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CourseViewHolder extends RecyclerView.ViewHolder {
+    public CourseViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
